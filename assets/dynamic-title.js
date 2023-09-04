c2ac59c3dc43dc5af09e138c4ab75be5d182d6ce
Zhihua Su <su_zhihua@tech.gov.sg>
2023-09-04 23:40:44 +0800

    change

--- assets/dynamic-title.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (typeof propertyTitle !== 'undefined' && propertyTitle) {
-        document.title = propertyTitle + ' | Huttons Asia Pte Ltd' ;
-    }
-});
--- assets/review-read-more.js
+++
@@ -1,9 +0,0 @@
-function toggleReviewText(id) {
-    var reviewText = document.getElementById("review-text-" + id);
-
-    if (reviewText.classList.contains("text-truncate")) {
-        reviewText.classList.remove("text-truncate");
-    } else {
-        reviewText.classList.add("text-truncate");
-    }
-}
